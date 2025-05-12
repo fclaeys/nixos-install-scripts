e6f6d43993b124085f486e49a00a0044184a9508
Fabrice Claeys <fabrice.claeys@symbol-it.fr>
2025-05-12 10:55:39 +0200

    configure to my usage

--- hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
+++ hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
@@ -66,7 +66,7 @@ echo '
 
   users.users.root.openssh.authorizedKeys.keys = [
     # Replace this by your SSH pubkey!
-    "ssh-rsa AAAAAAAAAAA..."
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIIuPSZCgGQltJPa6nQvr3RfRf/1uDdZiMypUS6Edfv5 fclaeys@nixos"
   ];
 }
 ' >> /mnt/etc/nixos/configuration.nix
